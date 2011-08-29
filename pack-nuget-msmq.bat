27a5202b198c3ecda95821097a426bfd39feb063
Daniel Crenna <daniel.crenna@gmail.com>
2011-08-29 10:24:12 -0400

    Added MSMQ

---
+++ pack-nuget-msmq.bat
@@ -0,0 +1,2 @@
+copy LICENSE bin
+..\NuGet.exe pack pvc-msmq.nuspec -b bin -o bin
---
+++ src/pvc.Adapters.MSMQ/MicrosoftQueue.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Messaging;
+using pvc.Core;
+
+namespace pvc.Adapters.MicrosoftMQ
+{
+    public class MicrosoftQueue<T> : IQueue<T>, IDisposable
+    {
+// ReSharper disable StaticFieldInGenericType
+        private static readonly IMessageFormatter _formatter;
+// ReSharper restore StaticFieldInGenericType
+
+        private readonly MessageQueue _receiveQueue;
+        private readonly MessageQueue _sendQueue;
+
+        static MicrosoftQueue()
+        {
+            _formatter = new BinaryMessageFormatter();
+        }
+
+        private MicrosoftQueue(MessageQueue queue)
+        {
+            _sendQueue = queue;
+        }
+
+        public MicrosoftQueue(string queueName)
+        {
+            _receiveQueue = _sendQueue = new MessageQueue(queueName) { Formatter = _formatter };
+        }
+
+        public MicrosoftQueue(string sendQueueNames, string receiveQueueName)
+        {
+            _sendQueue = new MessageQueue(sendQueueNames) { Formatter = _formatter };
+            _receiveQueue = new MessageQueue(receiveQueueName) { Formatter = _formatter };
+        }
+
+        public static MicrosoftQueue<T> Create(string queueName)
+        {
+            return new MicrosoftQueue<T>(new MessageQueue(queueName) { Formatter = new BinaryMessageFormatter() });
+        }
+
+        public void Dispose()
+        {
+            if (ReferenceEquals(_sendQueue, _receiveQueue) && (_receiveQueue != null))
+            {
+                _receiveQueue.Dispose();
+            }
+            if (_sendQueue != null)
+            {
+                _sendQueue.Dispose();
+            }
+        }
+
+        public void Enqueue(T item)
+        {
+            _sendQueue.Send(new System.Messaging.Message(item, _sendQueue.Formatter));
+        }
+
+        public virtual void MarkComplete(T item)
+        {
+
+        }
+
+        public void Requeue(T item)
+        {
+        }
+
+        public virtual bool TryDequeue(out T item)
+        {
+            using (var message = _receiveQueue.Receive())
+            {
+                if (message == null)
+                {
+                    item = default(T);
+                    return false;
+                }
+                message.Formatter = _receiveQueue.Formatter;
+                item = (T)message.Body;
+            }
+            return true;
+        }
+    }
+}
+
