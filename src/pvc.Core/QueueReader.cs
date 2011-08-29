ab260d3272642cb223f3ea28811759a13b13067d
Daniel Crenna <daniel.crenna@gmail.com>
2011-08-29 09:30:10 -0400

    Added pull request for queue reader from main repository; added log4net from NuGet

--- src/pvc.Core/QueueReader.cs
+++ src/pvc.Core/QueueReader.cs
@@ -1,35 +1,56 @@
 using System;
 using System.Threading;
+using log4net;
 
 namespace pvc.Core
 {
 	public class QueueReader<T> : Produces<T> where T : Message
 	{
-		private readonly IQueue<T> _queue;
+        private readonly ILog _logger = LogManager.GetLogger(typeof(QueueReader<T>));
+
+        private readonly IQueue<T> _queue;
 		private Consumes<T> _consumer;
+        private volatile bool _continue;
+		private Thread _thread;
+
+		public delegate void LogError(string format, params object[] args);
 
 		public QueueReader(IQueue<T> queue)
 		{
 			_queue = queue;
+		}
 
+		public QueueReader(IQueue<T> queue, ILog logger)
+		{
+			_queue = queue;
+			_logger = logger;
 		}
 
 		private void Run()
 		{
-			while (true)
+			while (_continue)
 			{
 				try
 				{
 					T item;
-					if(_queue.TryDequeue(out item))
+					if (_queue.TryDequeue(out item))
 					{
-						_consumer.Handle(item);
-						_queue.MarkComplete(item);
+						try
+						{
+							_consumer.Handle(item);
+							_queue.MarkComplete(item);
+						}
+						catch (Exception exception)
+						{
+						    var typeName = Equals(item, default(T)) ? "(null)" : item.GetType().Name;
+						    _logger.ErrorFormat("Error handling object of type {0}:\n{1}", typeName, exception);
+							// requeue?
+						}
 					}
 				}
-				catch (Exception)
+				catch (Exception exception)
 				{
-					//Todo: Add log4net
+					_logger.ErrorFormat("Error dequeuing object of type {0}:\n{1}", typeof(T).Name, exception);
 					//Dead letter
 					//Stop?
 					//??
@@ -39,8 +60,24 @@ namespace pvc.Core
 
 		public void Start()
 		{
-			var t = new Thread(Run) { IsBackground = true };
-			t.Start();
+			if (_thread != null)
+			{
+				throw new InvalidOperationException("Start called while reader already running.");
+			}
+			_thread = new Thread(Run) { IsBackground = true };
+			_thread.Start();
+		}
+
+		public void Stop()
+		{
+			var currentThread = _thread;
+			_thread = null;
+			if (currentThread == null)
+			{
+			    return;
+			}
+			_continue = false;
+			currentThread.Join();
 		}
 
 		public void AttachConsumer(Consumes<T> consumer)
@@ -48,6 +85,4 @@ namespace pvc.Core
 			_consumer = consumer;
 		}
 	}
-
-
 }
