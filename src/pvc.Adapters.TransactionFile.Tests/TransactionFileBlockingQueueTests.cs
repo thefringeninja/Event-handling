2f276f04dc3276650dfa84c15ff09909bdd76596
Daniel <DCrenna@CDIC.ca>
2011-09-28 13:11:16 -0400

    Adding back missing packages from a bad .gitignore file

---
+++ src/pvc.Adapters.TransactionFile.Tests/TransactionFileBlockingQueueTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using pvc.Adapters.TransactionFile.Queues;
+
+namespace pvc.Adapters.TransactionFile.Tests
+{
+    [TestFixture]
+    public class TransactionFileBlockingQueueTests
+    {
+        [Test]
+        public void Can_block_with_multiple_queue_consumers()
+        {
+            var queue = new TransactionFileBlockingQueue<object>("Can_block_with_multiple_queue_consumers.dat");
+            queue.Enqueue(new object());
+            queue.Enqueue(new object());
+            queue.Enqueue(new object());
+
+
+        }
+    }
+}
