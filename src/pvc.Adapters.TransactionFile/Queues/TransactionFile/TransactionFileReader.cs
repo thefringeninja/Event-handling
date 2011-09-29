457c62c816d66a89ee67f97597544ccb4a3dfe91
Daniel <DCrenna@CDIC.ca>
2011-09-29 15:48:02 -0400

    Limited file access of reader to Read, since sharing is still designated as ReadWrite

--- src/pvc.Adapters.TransactionFile/Queues/TransactionFile/TransactionFileReader.cs
+++ src/pvc.Adapters.TransactionFile/Queues/TransactionFile/TransactionFileReader.cs
@@ -9,9 +9,8 @@ namespace pvc.Adapters.TransactionFile.Queues.TransactionFile
 	/// <summary>
 	/// Reads transactions from the transaction file
 	/// </summary>
-	/// <typeparam name="T">The type of transactions to read, generally this will be a shared base class or
-	/// interface but could also be 'object' in order to allow untyped access</typeparam>
-	class TransactionFileReader<T>
+	/// <typeparam name="T">The type of transactions to read, generally this will be a shared base class or interface but could also be 'object' in order to allow untyped access</typeparam>
+	internal class TransactionFileReader<T>
 	{
 		protected FileStream _fileStream;
 		protected IFormatter _formatter;
@@ -54,7 +53,7 @@ namespace pvc.Adapters.TransactionFile.Queues.TransactionFile
                 _logger.Debug(string.Format("Opening Transaction File: {0}", filename));
 			}
 			
-            _fileStream = fi.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+            _fileStream = fi.Open(FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite);
 		}
 	}
 }
